3f1f2186655062e9cd38a2c428039d3e17289be8
brennanMKE <brennan@smallsharptools.com>
2017-02-21 18:17:11 -0800

    updates mapping playground with map/flatMap examples

--- StravaResultKit.playground/Pages/Mapping.xcplaygroundpage/Contents.swift
+++ StravaResultKit.playground/Pages/Mapping.xcplaygroundpage/Contents.swift
@@ -2,6 +2,7 @@
 import Foundation
 import CoreLocation
 import StravaKit
+import Result
 import StravaResultKit
 
 // Prepare for testing locally with alternative requestor.
@@ -13,13 +14,39 @@ Strava.configure(accessToken: accessToken, athleteDictionary: nil, alternateRequ
 
 // Mapping
 
+struct SimpleAthlete {
+    public let athleteId: Int
+    public let name: String
+    public let location: String
+}
+
+func mapSimpleAthlete(athlete: Athlete) -> SimpleAthlete {
+    let athleteId = athlete.athleteId
+    let name = "\(athlete.firstName) \(athlete.lastName)"
+    let location = "\(athlete.city), \(athlete.state) \(athlete.country)"
+    return SimpleAthlete(athleteId: athleteId, name: name, location: location)
+}
+
+func flatMapSimpleAthlete(athlete: Athlete) -> Result<SimpleAthlete, StravaResultError> {
+    return .success(mapSimpleAthlete(athlete: athlete))
+}
+
+// Map
+
 StravaResult.getAthlete { (result) in
-    result.flatMap({ (athlete) -> Result<Athlete, StravaResultError> in
-        return athlete
-    })
+    let athlete = result.map(mapSimpleAthlete)
+    print("map")
+    dump(athlete)
+}
+
+// Flat Map
+
+StravaResult.getAthlete { (result) in
+    let simpleResult = result.flatMap(flatMapSimpleAthlete)
+    print("flatMap")
+    dump(simpleResult)
 }
 
 // Playground Configuration
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
-
